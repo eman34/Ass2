208bf2c23083546050cc7d7bc45d2858f8a71f47
Edson Wu <edwu7920@uni.sydney.edu.au>
2021-05-05 02:38:58 +1000

    Tuesday

---
+++ MyClassifier.py
@@ -0,0 +1,10 @@
+import sys
+from node import Node
+
+#fn for 
+
+
+def main(args: list) -> None:
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ node.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, attr:list) -> None:
+        self.attr = list    #List of floats
+        self.val = None     #For class: True=Yes , False=No tested for diabetes
+        self.dist = None    #Store the k smallest nodes
+
+
+#Heidi:
+
